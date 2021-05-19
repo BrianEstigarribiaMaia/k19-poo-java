10afdaccb7074e5913a3bd2f93cae8fa4e11d24c
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-05-19 20:12:29 -0300

    Add files via upload

---
+++ cap-orientacaoaobjetos/TesteAgencia.java
@@ -0,0 +1,14 @@
+package br.com.k19poo.orientacaoaobjetos.testes;
+
+import br.com.k19poo.orientacaoaobjetos.Agencia;
+
+public class TesteAgencia {
+
+	public static void main(String[] args) {
+		Agencia agencia1 = new Agencia(1234);
+		Agencia agencia2 = new Agencia(4567);
+		
+		System.out.println(agencia1);
+		System.out.println(agencia2);
+	}
+}
