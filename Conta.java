b7a7007ded5b5a3cfe1812b7f22dd1c6daad35ac
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-06-02 16:56:11 -0300

    Add files via upload

---
+++ Conta.java
@@ -0,0 +1,16 @@
+package br.com.k19poo.polimorfismo;
+
+public class Conta {
+
+	private double saldo;
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+}
