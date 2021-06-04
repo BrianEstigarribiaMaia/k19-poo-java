4c7308314931e50a1596f1d313c16056eb1791d8
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-06-03 21:33:16 -0300

    Add files via upload

---
+++ cap-polimorfismo/Gerente.java
@@ -0,0 +1,19 @@
+package br.com.k19poo.polimorfismo;
+
+public class Gerente extends Funcionario{
+
+	private String usuario, senha;
+	
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
---
+++ cap-polimorfismo/Telefonista.java
@@ -0,0 +1,14 @@
+package br.com.k19poo.polimorfismo;
+
+public class Telefonista extends Funcionario{
+
+	private int ramal;
+
+	public int getRamal() {
+		return ramal;
+	}
+
+	public void setRamal(int ramal) {
+		this.ramal = ramal;
+	}	
+}
